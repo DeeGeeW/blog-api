443cd183d6cc63158c46e3317a0d94125d6821f4
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-15 22:27:27 -0400

    added a test for rspec practice

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
@@ -7,8 +7,6 @@ RSpec.describe "Posts", type: :request do
 
       Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/852/200/300.jpg?hmac=6IMZOkPF_q5nf8IwfYdfxPUyKnyPL1w8moDjTeMOT5g")
       Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/135/200/300.jpg?hmac=d3sTOCUkxdC1OKCgh9wTPjck-gMWATyVHFvflla5vLI")
-
-
 
       get "/posts"
       posts = JSON.parse(response.body)
@@ -19,7 +17,7 @@ RSpec.describe "Posts", type: :request do
   describe "GET posts/:id" do
     it "show if post attributes work correctly" do
       user = User.create!(name: "Test", email: "test@test.com", password: "password")
-      
+
       post = Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/852/200/300.jpg?hmac=6IMZOkPF_q5nf8IwfYdfxPUyKnyPL1w8moDjTeMOT5g")
 
       get "/posts/#{post.id}"
@@ -33,37 +31,37 @@ RSpec.describe "Posts", type: :request do
   end
   describe "POST /posts" do
     it "creates new post" do
-      
       user = User.create!(name: "Test", email: "test@test.com", password: "password")
-      
+
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i, 
+          exp: 24.hours.from_now.to_i,
         },
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
-        "HS256" 
+        "HS256"
       )
 
       post "/posts", params: {
-        user_id: user.id,
-        title: "title test",
-        body: "body test",
-        image: "image string test"
-      },
-      headers: {"Authorization" => "Bearer #{jwt}"}
+                       user_id: user.id,
+                       title: "title test",
+                       body: "body test",
+                       image: "image string test",
+                     },
+                     headers: { "Authorization" => "Bearer #{jwt}" }
 
       post = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
       expect(post["title"]).to eq("title test")
+      expect(post["body"]).to eq("body test")
     end
   end
 end
 # RSpec.describe "Users", type: :request do
 #   describe "GET /users" do
 #     it "returns an array of users" do
-      
+
 #       User.create!(name: "Peter", email: "peter@test.com", password: "password")
 #       User.create!(name: "Test", email: "test@test.com", password: "password")
 
@@ -74,7 +72,3 @@ end
 #     end
 #   end
 # end
-
-  
-
-
